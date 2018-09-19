b0e0d6271e8c2881f850d33ce62324cada563ad1
peeb999 <peter.bishay01@gmail.com>
2018-09-19 21:09:21 +1000

    Dynamically load subjects

--- src/components/Secure.vue
+++ src/components/Secure.vue
@@ -91,64 +91,17 @@
                     </ol>
                     <!-- End breadcrumbs -->
                     <!-- Subjects -->
-                    <div class="row">
-                        <div class="col-xl-3 col-sm-6 mb-3">
-                            <div class="card text-white bg-primary o-hidden h-100">
+                    <!-- If there are subjects -->
+                    <div v-if="subjects.length > 0" class="row">
+                        <div v-for="(subject, index) in subjects" class="col-xl-3 col-sm-6 mb-3" :key="index">
+                            <div :class="'card text-white ' + colours[index%colours.length] + ' o-hidden h-100'">
                                 <div class="card-body">
                                     <div class="card-body-icon">
                                         <i class="fas fa-fw fa-comments"></i>
                                     </div>
-                                    <div class="mr-5">COMP1111</div>
+                                    <div class="mr-5">{{subject.name}}</div>
                                 </div>
-                                <a class="card-footer text-white clearfix small z-1" href="#">
-                                    <span class="float-left">View Details</span>
-                                    <span class="float-right">
-                                        <i class="fas fa-angle-right"></i>
-                                    </span>
-                                </a>
-                            </div>
-                        </div>
-                        <div class="col-xl-3 col-sm-6 mb-3">
-                            <div class="card text-white bg-warning o-hidden h-100">
-                                <div class="card-body">
-                                    <div class="card-body-icon">
-                                        <i class="fas fa-fw fa-list"></i>
-                                    </div>
-                                    <div class="mr-5">COMP2222</div>
-                                </div>
-                                <a class="card-footer text-white clearfix small z-1" href="#">
-                                    <span class="float-left">View Details</span>
-                                    <span class="float-right">
-                                        <i class="fas fa-angle-right"></i>
-                                    </span>
-                                </a>
-                            </div>
-                        </div>
-                        <div class="col-xl-3 col-sm-6 mb-3">
-                            <div class="card text-white bg-success o-hidden h-100">
-                                <div class="card-body">
-                                    <div class="card-body-icon">
-                                        <i class="fas fa-fw fa-shopping-cart"></i>
-                                    </div>
-                                    <div class="mr-5">COMP3333</div>
-                                </div>
-                                <a class="card-footer text-white clearfix small z-1" href="#">
-                                    <span class="float-left">View Details</span>
-                                    <span class="float-right">
-                                        <i class="fas fa-angle-right"></i>
-                                    </span>
-                                </a>
-                            </div>
-                        </div>
-                        <div class="col-xl-3 col-sm-6 mb-3">
-                            <div class="card text-white bg-danger o-hidden h-100">
-                                <div class="card-body">
-                                    <div class="card-body-icon">
-                                        <i class="fas fa-fw fa-life-ring"></i>
-                                    </div>
-                                    <div class="mr-5">COMP4444</div>
-                                </div>
-                                <a class="card-footer text-white clearfix small z-1" href="#">
+                                <a v-on:click="next(subject, $event)" class="card-footer text-white clearfix small z-1" href="">
                                     <span class="float-left">View Details</span>
                                     <span class="float-right">
                                         <i class="fas fa-angle-right"></i>
@@ -157,11 +110,15 @@
                             </div>
                         </div>
                     </div>
+                    <!-- If there aren't subjects-->
+                    <div v-else class="row">
+                        <span>No subjects, please add a subject</span>
+                    </div>
                     <!-- End subjects -->
                     <div class="row">
                         <!-- Calendar -->
                         <div class="col-lg-7">
-                            <div class="card mb-3">
+                            <div class="card mb-3"> 
                                 <div class="card-header">Calendar</div>
                                 <div class="card-body">
                                     <canvas id="myBarChart" width="100%" height="50"></canvas>
@@ -216,8 +173,45 @@
     export default {
         name: 'Secure',
         data() {
-            return {};
+            return {
+                subjects: [
+                    // {name: "COMP9323"},
+                    // {name: "COMP1234"},
+                    // {name: "COMP1232"},
+                    // {name: "COMP1231"}
+                ],
+                colours: [
+                    "bg-primary",
+                    "bg-warning",
+                    "bg-success",
+                    "bg-danger"
+                ]
+            };
+        },
+        methods: {
+            next(subject, event) {
+                event.preventDefault();
+                alert(JSON.stringify(subject));
+            }
+        },
+        mounted() {
+            fetch(`http://localhost:8081/subjects/${this.$parent.user_id}`, {
+                        method: 'GET',
+                    }).then(response => {
+                        if (response.status === 200) {
+                            response.json().then(subs => {
+                                subs.forEach(subject => {
+                                    this.subjects.push(subject);
+                                });
+                                
+                            })                           
+                        } else {
+                            console.log("Cannot retrieve subjects");
+                        }
+            });
+                
         }
+        
     }
 </script>
 
