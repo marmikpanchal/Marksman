84192ddcbe66460eb09d3c01ebc07d3d8d5f69df
royaranda <royaranda2@gmail.com>
2018-09-20 01:58:16 +1000

    Fixed styling for Register page

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -17,61 +17,58 @@
     </div>
 </template> -->
 
+
 <template>
-  <v-layout align-center justify-center fill-height>
-    <v-flex xs12 sm10 md8 lg6>
-      <v-card ref="form">
-        <v-card-text>
-          <v-text-field
-            :error-messages="errorMessages"
-            label="Username"
-            placeholder="marksmanuser"
-            required
-            name="username" 
-            v-model="input.username" 
-          ></v-text-field>
-          <v-text-field
-            label="Password"
-            required
-            type="password" 
-            name="password" 
-            v-model="input.password" 
-            placeholder="Password"
-          ></v-text-field>
-          <v-text-field
-            label="Email"
-            placeholder="marksman@gmail.com"
-            required
-            type="email" 
-            name="email" 
-            v-model="input.email"
-          ></v-text-field>
-        </v-card-text>
-        <v-divider class="mt-5"></v-divider>
-        <v-card-actions>
-          <v-btn flat>Cancel</v-btn>
-          <v-spacer></v-spacer>
-          <v-slide-x-reverse-transition>
-            <v-tooltip
-              v-if="formHasErrors"
-              left
-            >
-              <v-btn
-                slot="activator"
-                icon
-                class="my-0"
-                @click="resetForm"
-              >
-                <v-icon>refresh</v-icon>
-              </v-btn>
-              <span>Refresh form</span>
-            </v-tooltip>
-          </v-slide-x-reverse-transition>
-          <v-btn color="primary" v-on:click="registerAccount()">Register</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-flex>
-  </v-layout>
+  <v-app id="inspire">
+      <h1>Marksman</h1>
+    <v-content>
+      <v-container fluid fill-height>
+        <v-layout align-center justify-center>
+          <v-flex xs12 sm8 md4>
+            <v-card class="elevation-12">
+              <v-toolbar dark color="purple">
+                <v-toolbar-title>Become a Marksman</v-toolbar-title>
+                <v-spacer></v-spacer>
+                
+              </v-toolbar>
+              <v-card-text>
+                <v-form>
+                  <v-text-field 
+                    prepend-icon="person" 
+                    name="username" 
+                    type="text" 
+                    v-model="input.username" 
+                    label="Username"
+                    placeholder="Create a username"></v-text-field>
+                  <v-text-field 
+                    id="password" 
+                    prepend-icon="lock" 
+                    name="password" 
+                    type="password" 
+                    v-model="input.password" 
+                    label="Password"
+                    placeholder="Create a password"
+                    required></v-text-field>
+                  <v-text-field 
+                    prepend-icon="email" 
+                    name="email" 
+                    type="email" 
+                    label="Email" 
+                    placeholder="eg. marksman@gmail.com"
+                    required
+                    v-model="input.email"></v-text-field>
+                </v-form>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="purple" v-on:click="registerAccount()">Register</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
@@ -126,5 +123,11 @@
         border: 1px solid #CCCCCC;
         padding: 20px;
         margin-top: 10px;
+    }
+
+    h1 {
+        text-align: center;
+        padding-top: 80px;
+        font-size: 48px;
     }
 </style>
