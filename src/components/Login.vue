931aba12662480b0a9ccc638a78efc0bba445ed2
Marmik Panchal <marmik.panchal@gmail.com>
2018-09-20 15:32:01 +1000

    Added Fb-login button and functionality

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -6,17 +6,17 @@
         <v-layout align-center justify-center>
           <v-flex xs12 sm8 md4>
             <v-card class="elevation-12">
+              
               <v-toolbar dark color="primary">
                 <v-toolbar-title>Login</v-toolbar-title>
                 <v-spacer></v-spacer>
-                <v-tooltip bottom>
-                  <v-btn
-                    slot="activator"
-                    :href="source"
-                    icon
-                    large
-                    target="_blank"
-                  >
+                Sign in with:
+                <fb-signin-button :params="fbSignInParams" @success="onSignInSuccess" @error="onSignInError">
+                    Facebook
+                </fb-signin-button>
+                
+               <!--  <v-tooltip bottom>
+                  <v-btn slot="activator" :href="source" icon large target="_blank">
                     <v-icon medium></v-icon>
                   </v-btn>
                   <span>Login with Facebook</span>
@@ -26,20 +26,22 @@
                     <v-icon medium></v-icon>
                   </v-btn>
                   <span>Login with Google</span>
-                </v-tooltip>
+                </v-tooltip> -->
               </v-toolbar>
+              
               <v-card-text>
                 <v-form>
                   <v-text-field prepend-icon="person" name="login" type="text" v-model="input.username" label="Username"></v-text-field>
                   <v-text-field id="password" prepend-icon="lock" name="password" type="password" v-model="input.password" label="Password"></v-text-field>
                 </v-form>
               </v-card-text>
+
               <v-card-actions>
-                
                 <v-spacer></v-spacer>
                 <v-btn v-on:click="register()" depressed target="_blank">Register</v-btn>
                 <v-btn color="primary" v-on:click="login()">Login</v-btn>
               </v-card-actions>
+            
             </v-card>
           </v-flex>
         </v-layout>
@@ -78,6 +80,8 @@
             }
         },
         methods: {
+            
+
             login() {
                 if(this.input.username != "" && this.input.password != "") {
                     const email = '';
@@ -138,5 +142,18 @@
         margin: auto;
         margin-top: 200px;
         padding: 20px;
+    }
+
+    .fb-signin-button {
+        margin-left: 10px;
+        width: 100px;
+        text-align: center;
+        height: 30px;
+        display: inline-block;
+        padding: 4px 8px;
+        border-radius: 3px;
+        background-color: #4267b2;
+        color: #fff;
+        cursor: pointer;
     }
 </style>
--- src/main.js
+++ src/main.js
@@ -9,15 +9,34 @@ import 'material-design-icons-iconfont/dist/material-design-icons.css'
 import '../node_modules/jquery/dist/jquery.min.js';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/bootstrap/dist/js/bootstrap.min.js';
+import FBSignInButton from 'vue-facebook-signin-button'
 
 Vue.config.productionTip = false
-
+Vue.use(FBSignInButton)
 Vue.use(Vuetify)
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
+	el: '#app',
+	router,
+	created: function(){
+		window.fbAsyncInit = function() {
+		    FB.init({
+		        appId            : '533799663736208',
+		        autoLogAppEvents : true,
+		        xfbml            : true,
+		        version          : 'v3.1'
+		    });
+		};
+
+		(function(d, s, id){
+		    var js, fjs = d.getElementsByTagName(s)[0];
+		    if (d.getElementById(id)) {return;}
+		    js = d.createElement(s); js.id = id;
+		    js.src = "https://connect.facebook.net/en_US/sdk.js";
+		    fjs.parentNode.insertBefore(js, fjs);
+		}(document, 'script', 'facebook-jssdk'));
+	},
+	components: { App },
+	template: '<App/>'
 })
